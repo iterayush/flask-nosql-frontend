9aa238cc8622a8b9b985acb2752d037c0f31a990
Ayush Kumar Singh <ayushsingh.bui@gmail.com>
2024-10-13 23:08:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:5000/api/users';
+const baseUrl = 'https://flask-nosql-backend.onrender.com/api/users';
 
 const UserManagement = () => {
   const [users, setUsers] = useState([]);
